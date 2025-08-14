147654faf722df4d35b65f0fec366cba98165acd
AironRuda <airon.rudaa@gmail.com>
2025-08-14 18:54:27 -0500

    feat: prisma set up

---
+++ prisma/migrations/20250814193825_add_orders/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."Order" (
+    "id" SERIAL NOT NULL,
+    "items" JSONB NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
